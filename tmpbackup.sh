505ab551f38058fd0ccf6f69683f782589cb252a
stinkfist <stinkfist@stinkfist.localdomain>
2020-11-12 13:40:24 -0400

    Changes in variables and addition of user input
    added user input for path selection, thus deprecating the usage of the '/home/stinkfist/Downloads/scripts' env var inside the previous build_p local variable

--- tmpbackup.sh
+++ tmpbackup.sh
@@ -2,16 +2,14 @@
 
 # Program variables
 
-build_b="$PWD/tmpbackup"
 SBo="compiled_pkg"
 
 # Root check
 [[ "$UID" -gt "0" ]] && { echo "Permission denied"; exit 1 ;}
 
-# build_b dir check
-
 # Functions
 function backup(){
+	read -p "Please set up a directory to store your backup (path/to/backup): " build_b
 	[[ ! -d "$build_b" ]] && { echo "creating backuptmp directory..."; mkdir "$build_b" ;}
 	if cd /tmp; then
 	tar -cvzf "$SBo" *_SBo.tgz && mv -v "$SBo" "$build_b"
