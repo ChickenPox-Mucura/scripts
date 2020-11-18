4ddf60c546432fb899de54cbe26d8e030e8b20a0
stinkfist <stinkfist@stinkfist.localdomain>
2020-11-18 12:11:22 -0400

    new script added
    script to run the 'translate' program; translates english into portuguese.

---
+++ translator.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Welcome to Google Translate!"
+echo "Verifying your internet connection..."
+
+if wget -q --spider www.pudim.com.br; then
+	echo ""
+	clear
+else
+	{ echo "No internet connection"; sleep 2s; clear; exit 1 ;}
+fi
+
+read -p "Word: " _word
+
+if [[ -n "$_word" ]]; then
+	translate -t portuguese -f english "$_word"
+fi
