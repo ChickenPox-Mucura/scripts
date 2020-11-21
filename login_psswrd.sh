30daf8e2a92837dcb2ccd326d64e590d0321f58e
stinkfist <stinkfist@stinkfist.localdomain>
2020-11-21 19:31:12 -0400

    Changes in function
    A while loop was added to make a limited number of login attempts possible; menu programs were changed; unnecessary lines of code inside the function were also deleted.

--- login_psswrd.sh
+++ login_psswrd.sh
@@ -1,36 +1,32 @@
 #!/usr/bin/env bash
 
-# Source .conf file
-source /home/stinkfist/sys-log/login_psswrd.conf
-variables
-source /home/stinkfist/sys-log/libhelp
+# Variables
+user="stinkfist"
+pass="1234"
+total="0"
+
 # Functions======================#
 function login(){
+	while true; do
 		read -p "Login: " _login
-		read -t 5 -sp "Password: " _pass
+		read -p "Password: " _pass
 		if [[ "$_login" = "stinkfist" ]] && [[ "$_pass" -eq "$pass" ]]; then
-			echo ""
 			sleep 1s
+			break
 			clear
-		elif [[ "$_login" != "$user" ]]; then
-			echo "Wrong username or password"
-			sleep 0.5s
-			exit 1
-		elif [[ "$_pass" != "$pass" ]]; then
-			echo "Wrong username or password"
-			sleep 0.5s
-			exit 1
+		else
+			let total++
+			[[ "$total" -eq "3" ]] && { echo "3 failed attempts. Exiting..."; sleep 1s; exit 0 ;} \
+			|| echo "Login or password incorrect. Attempts: $total/3"
 		fi
-
+	done
 
 		echo \
-		"+++++++++Welcome, $user++++++++
-		1) Leafpad
-		2) XMMS
-		3) cacafire
-		4) Leave
-		+++++++++++++++++++++++++++++++++++
-		"
+			"[Welcome, $user]
+			1) Leafpad
+			2) Gedit
+			3) Sublime
+			4) Leave"
 
 
 		read -p "Select an option: " option
@@ -38,19 +34,28 @@ function login(){
 		case "$option" in
 			1)
 				[[ $(type -P leafpad) ]] || { echo "leafpad not found!"; exit 1 ;}
-				leafpad & 
+				leafpad &
 			;;
 
-			2) xmms ;;
-			3) cacafire & ;;
-			4) clear && exit 0 ;;
+			2)
+				[[ $(type -P gedit) ]] || { echo "gedit not found!"; exit 1 ;}
+				gedit &
+			;;
+			3)
+				[[ $(type -P sublime_text) ]] || { echo "sublime not found"; exit 1 ;}
+				sublime_text &
+			;;
+			4)
+				clear
+				exit 0
+			;;
 		esac
 
 }
 
 case "$1" in
-	-l|--login) 
-		login		
+	-l|--login)
+		login
 		;; # Closes -l block, --login
 
 		-h|--help)
