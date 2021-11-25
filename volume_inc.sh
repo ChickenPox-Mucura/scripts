399f60ce00cf38c3e18c1d1abb3ac6b512398d0a
Mucura <thefourthpillar@protonmail.com>
2021-11-25 17:04:54 -0400

    volume increase/decrease scripts added

---
+++ volume_inc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+
+amixer set Master 5%+ && dunstify -h string:x-canonical-private-synchronous:screen "Volume `amixer get Master | grep Left | tail -1 | cut --delimiter=' ' -f 7` " -h int:value:"`amixer get Master | grep Left | tail -1 | cut --delimiter=' ' -f 7 | sed s/[[:punct:]]//g`" 
