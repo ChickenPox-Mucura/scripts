d0bcf9ee3d5d9c58999511f1cba1e39164147eaf
Mucura <thefourthpillar@protonmail.com>
2021-12-05 21:19:06 -0400

    specified a different card for amixer
    output filterinf modified

--- volume_inc.sh
+++ volume_inc.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-amixer set Master 5%+ && dunstify -h string:x-canonical-private-synchronous:screen "Volume `amixer get Master | grep Left | tail -1 | cut --delimiter=' ' -f 7` " -h int:value:"`amixer get Master | grep Left | tail -1 | cut --delimiter=' ' -f 7 | sed s/[[:punct:]]//g`" 
+amixer -c 1 set Master 5%+ && dunstify -h string:x-canonical-private-synchronous:screen "Volume `amixer -c 1 get Master | grep dB | cut --delimiter=' ' -f 6` " -h int:value:"`amixer -c 1 get Master | grep dB | cut --delimiter=' ' -f 6 | sed s/[[:punct:]]//g` " 
